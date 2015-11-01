349b0a8bd902d7f17aa628fd825887a775dca39b
Khushhall <kcm00000@gmail.com>
2015-11-02 02:43:27 +0530

    Added Q1, coin problem

---
+++ DP/Q1.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int minCoins(int coins[], int n,int sum){
+	int *table = new int[sum+1];
+	int out;
+	table[0] = 0; 
+
+	for (int i = 1;i <= sum;i++)
+		table[i] = INT_MAX;
+
+	for (int i = 1;i <= sum;i++){
+		for (int j=0; j<n; j++){
+			if (coins[j] <= i){
+				int temp = table[i-coins[j]];
+				if(temp != INT_MAX && temp+1 < table[i])
+					table[i] = temp+1;
+			}
+		}          
+	}
+	
+	out = table[sum];
+	delete[] table;
+	return out;
+}
+
+int main()
+{
+	int coins[] =  {1, 3, 5};
+    int n = sizeof(coins)/sizeof(coins[0]);
+    int sum= 131300000;
+    cout << "Minimum coins required is \n"<< minCoins(coins, n, sum)<<"\n";
+    return 0;
+}
+
