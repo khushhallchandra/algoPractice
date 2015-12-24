b42c2737b972a74442216e19e0469e323bf34579
khushhallchandra <kcm00000@gmail.com>
2015-12-24 20:12:33 +0530

    Completed heap sort

--- sorting/heapsort.cpp
+++ sorting/heapsort.cpp
@@ -4,35 +4,65 @@
 
 using namespace std;
 
-void h_sort(vector<int>& data,int n){
-	int val,j;
-	for(int i=1;i<n;i++){
-		val = data[i];
-		for(j=i-1; j>=0;j--){
-			if(val < data[j] ){
-				data[j+1] = data[j];
-			}
-			else				
-				break;
-			data[j] = val;
-		}
-	}
+void max_heapify(vector<int>& data, int i, int n){
+    int j, temp;
+    temp = data[i];
+    j = 2*i;
+
+    while (j <= n){
+    	if (j < n && data[j+1] > data[j])
+            j = j+1;
+
+        if (temp > data[j])
+            break;
+
+        else
+        if (temp <= data[j]){
+        	data[j/2] = data[j];
+            j = 2*j;
+        }
+    }
+    data[j/2] = temp;
+}
+
+void h_sort(vector<int>& data, int n){
+    int i, temp;
+
+    for (i = n; i >= 2; i--){
+        temp = data[i];
+        data[i] = data[1];
+        data[1] = temp;
+        max_heapify(data, 1, i - 1);
+    }
+}
+
+void build_maxheap(vector<int>& data, int n){
+    int i;
+
+    for(i = n/2; i >= 1; i--){
+        max_heapify(data, i, n);
+    }
 }
 
 int main(){
-	vector<int> vec;
-	int i;
-	std::ifstream infile("input.txt");
-	int val;
-	while (infile >> val)
-	{
-		vec.push_back(val);    
-	}
-	int n=vec.size();
-	h_sort(vec,n);
-	cout << "Sorted data:" << endl;
-	for(int i=0;i<n;i++){
-		cout<<vec[i] << endl;		
-	}
-	return 0;
+      vector<int> vec;
+      int i;
+      std::ifstream infile("input.txt");
+      int val;
+
+      vec.push_back(-1); // just storing some value at 0 position
+      while (infile >> val){
+            vec.push_back(val);    
+      }
+      int n=vec.size();
+      n--;
+
+      build_maxheap(vec,n);
+      h_sort(vec,n);
+      cout << "Sorted data:" << endl;
+      
+      for(int i=1;i<=n;i++){
+            cout<<vec[i] << endl;         
+      }
+      return 0;
 }
