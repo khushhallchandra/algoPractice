f508ce7276ca14621636967e12414e23f2faa472
khushhallchandra <kcm00000@gmail.com>
2016-01-13 23:46:13 +0530

    Added deque

---
+++ STL&Templates/deque.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <deque>
+#include <algorithm>
+#include <iterator>
+using namespace std;
+
+int main(){
+    deque <int> deq;
+
+    deq.assign (5, 1);
+    deq.push_back (2);
+    deq.push_front (3);
+
+    deque<int>::const_iterator pos = deq.begin();
+    while( pos != deq.end()) {
+        cout << "value in deque = " << *pos << endl;
+        pos++;
+    }
+
+    deq.pop_front();
+    deq.pop_back();
+    pos = deq.begin();
+    cout<<"Print again"<<endl;
+    while( pos != deq.end()) {
+        cout << "value in deque = " << *pos << endl;
+        pos++;
+    }    
+
+}
