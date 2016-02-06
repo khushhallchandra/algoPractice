e7f91149409c07d87b7f23000fc3301a66165014
khushhallchandra <kcm00000@gmail.com>
2016-02-07 03:00:34 +0530

    Completed HS12MBR, got 10 points

--- spoj/HS12MBR.cpp
+++ spoj/HS12MBR.cpp
@@ -2,8 +2,28 @@
 
 using namespace std;
 
+int lx=0,ly=0,rx=0,ry=0;
+
+void point(int x, int y){
+    if(!(lx||ly||rx||ry))
+        lx=x,rx=x,ly=y,ry=y;
+    else
+    if(((x<lx)||(x>rx))||((y<ly)||(y>ry))){
+        if(x<lx)
+            lx=x;
+        else
+        if(x>rx)
+            rx=x;
+        if(y<ly)
+            ly=y;
+        else
+        if(y>ry)
+            ry=y;             
+    }    
+
+}
 int main() {
-    int T,N,lx=0,ly=0,rx=0,ry=0;
+    int T,N;
     char ob;
     cin>>T;
     
@@ -15,60 +35,24 @@ int main() {
             if(ob=='p'){
                 int x,y;
                 cin>>x>>y;
-                if(!(lx||ly||rx||ry))
-                    lx=x,rx=x,ly=y,ry=y;
-                else
-                if(((x<lx)||(x>rx))||((y<ly)||(y>ry))){
-                    if(x<lx)
-                        lx=x;
-                    else
-                    if(x>rx)
-                        rx=x;
-                    if(y<ly)
-                        ly=y;
-                    else
-                    if(y>ry)
-                        ry=y;             
-                }
-                
+                point(x,y);                
             }
             else
             if(ob=='c'){
                 int x,y,r;
                 cin>>x>>y>>r;
-                if(!(lx||ly||rx||ry))
-                    lx=x-r,rx=x+r,ly=y-r,ry=y+r;
-                else
-                if(((x-r<lx)||(x+r>rx))||((y-r<ly)||(y+r>ry))){
-                    if(x-r<lx)
-                        lx=x-r;
-                    if(x+r>rx)
-                        rx=x+r;
-                    if(y-r<ly)
-                        ly=y-r;
-                    if(y+r>ry)
-                        ry=y+r;             
-                }                
+                point(x-r,y-r);
+                point(x+r,y+r);                
+                 
             }            
             else
             if(ob=='l'){
                 int x1,y1,x2,y2;
                 cin>>x1>>y1>>x2>>y2;
-                if(!(lx||ly||rx||ry))
-                    lx=x1,rx=x2,ly=y1,ry=y2;
-                else
-                if(((x1<lx)||(x2>rx))||((y1<ly)||(y2>ry))){
-                    if(x1<lx)
-                        lx=x1;
-                    if(x2>rx)
-                        rx=x2;
-                    if(y1<ly)
-                        ly=y1;
-                    if(y2>ry)
-                        ry=y2;             
-                }                                
-            } 
- 
+                point(x1,y1);
+                point(x2,y2);
+                               
+            }  
         }
         cout<<lx<<" "<<ly<<" "<<rx<<" "<<ry<<endl;
     }
