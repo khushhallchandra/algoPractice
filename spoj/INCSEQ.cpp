e98001d9569d156ddbff3b643d422ce91cc8b202
Khushall Chandra Mahajan <kcm00000@gmail.com>
2016-07-01 22:48:09 +0530

    Added DP

---
+++ spoj/INCSEQ.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define MAXN 100001
+
+int arr[MAXN];
+
+int main(){
+	int N,K;
+	cin>>N>>K;
+ 	while(N--)
+ 	  cin>>arr[N];
+  
+  return 0;
+}
+ 	
