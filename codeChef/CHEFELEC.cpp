d75e838699a7e2057c31c45ebe755da07d7b75a6
Khushall Chandra Mahajan <kcm00000@gmail.com>
2016-07-09 22:33:39 +0530

    Create CHEFELEC.cpp

---
+++ codeChef/CHEFELEC.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+ 
+int main(){
+    int T,N;
+    string s;
+    cin>>T;
+    while(T--){
+    	vector <int> pos,neg;
+    	
+    	cin>>N;
+    	cin>>s;
+
+ 	}
+ 	return 0;        
+} 
