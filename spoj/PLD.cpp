82c6b7730ac26ffeba652a2bbf88f4f8d8289cf4
Khushall Chandra Mahajan <kcm00000@gmail.com>
2016-07-19 23:46:39 +0530

    Update PLD.cpp

--- spoj/PLD.cpp
+++ spoj/PLD.cpp
@@ -3,11 +3,27 @@
 using namespace std;
 
 int main(){
-    int T, n;
-    cin>>T;
-    while(T--){
-        cin>>n;
-        
+    int k,tem,count =0,s,e;
+    cin>>k;
+    string str;
+    cin>>str;
+    int l = str.length();
+   	tem = l-k;
+    for(int i=0;i<=tem;i++){
+    	s=i;
+    	e=i+k-1;
+    	count++;
+    	while(s<=e){
+    		if(str[s]==str[e]){
+    			s++;
+    			e--;
+    		}
+    		else{
+    			count--;
+    			break;
+    		}
+    	}
     }
+    cout<<count;
     return 0;
 }
