239e85e4afa7320be7d9ff707d01d3e13d4b4b2c
Khushall Chandra Mahajan <kcm00000@gmail.com>
2016-07-22 23:26:02 +0530

    Update SARRAY.cpp

--- spoj/SARRAY.cpp
+++ spoj/SARRAY.cpp
@@ -1,13 +1,33 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
-int main(){
-    int T, n;
-    cin>>T;
-    while(T--){
-        cin>>n;
-        
-    }
-    return 0;
+int main() {
+	int temp;
+	vector<string> arr[62];
+	string str;
+	cin>>str;
+	int len = str.length();
+	for(int i =len-1;i>=0;i--){
+		if(str[i]>='0'&&str[i]<='9')
+			temp = str[i]-'0';
+		else
+		if(str[i]>='A'&&str[i]<='Z')
+			temp = str[i]-'A' + 10;
+		else
+		if(str[i]>='a'&&str[i]<='z')
+			temp = str[i]-'a' + 36;
+		arr[temp].push_back(str.substr(i));
+	}
+	
+	for(int i=0;i<62;i++)
+		sort(arr[i].begin(),arr[i].end()); 
+	
+	for(int i=0;i<62;i++)
+		for(int j=0;j<arr[i].size();j++)
+		    cout<<len-arr[i][j].size()<<endl;	
+
+	return 0;
 }
