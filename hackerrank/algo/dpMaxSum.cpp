071ee43770b3b2f11a35201eb66a7d782afc8ae9
Khushhall <kcm00000@gmail.com>
2016-08-06 23:59:05 +0530

    Changed

--- hackerrank/algo/dpMaxSum.cpp
+++ hackerrank/algo/dpMaxSum.cpp
@@ -7,18 +7,26 @@ int main(){
 	cin>>T;
 	while(T--){
 		cin>>n;
-		int sum1=0, sum2=0, i, temp, max_ending_here = 0;
+		int sum1=0, sum2=0, i, temp, max_ending_here = 0, allNeg=1,s= -10000;
 		for(i=0;i<n;i++){
 			cin>>temp;
-			if(temp>0)
+			if(temp>0){
 				sum2 += temp;
+				allNeg = 0;
+			}
+			if(allNeg)
+				s = (s>temp)?s:temp;
+
 			max_ending_here += temp;
         	if (max_ending_here < 0)
             	max_ending_here = 0;
         	if (sum1 < max_ending_here)
             	sum1 = max_ending_here;
 		}
-		cout<<sum1<<" "<<sum2<<"\n";
+		if(allNeg)
+			cout<<s<<" "<<s<<"\n";
+		else
+			cout<<sum1<<" "<<sum2<<"\n";
 	}
 	return 0;
 }
