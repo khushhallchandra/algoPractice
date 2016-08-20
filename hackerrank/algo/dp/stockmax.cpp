4625c3c6e26c45068097f86e121f14bae971ced6
Khushhall <kcm00000@gmail.com>
2016-08-20 23:58:28 +0530

    Added

---
+++ hackerrank/algo/dp/stockmax.cpp
@@ -0,0 +1,41 @@
+//https://www.hackerrank.com/challenges/stockmax
+#include <bits/stdc++.h>
+
+using namespace std;
+ 
+int main()
+{
+    int T,N,price,temp;
+    cin>>T; 
+    while(T--){
+        cin>>N; 
+        vector <int> stock_price;
+        for (int i=0; i<N; ++i){
+            cin>>price;
+            stock_price.push_back(price);
+        }
+        if(N==0 || N==1) 
+            continue;
+        temp = 0;
+        long long int out = 0;
+        // for (int i=N-1; i>=0; --i){
+        //     temp = max(temp, stock_price[i]);
+        //     out += temp - stock_price[i];
+        // }
+
+
+
+        temp = stock_price[N-1];
+        for(int i = N-1; i >= 0; i--){
+            if(stock_price[i] > temp){
+                temp = stock_price[i];
+            }            
+            out += (temp - stock_price[i]);
+}
+
+
+
+        cout<<out<<endl;
+    }
+  return 0;
+}
