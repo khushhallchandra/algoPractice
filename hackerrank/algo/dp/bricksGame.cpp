7dcb749d56f609bec23bc24b8a1e661ae941f41c
Khushhall <kcm00000@gmail.com>
2016-08-22 23:53:13 +0530

    Added

--- hackerrank/algo/dp/bricksGame.cpp
+++ hackerrank/algo/dp/bricksGame.cpp
@@ -4,24 +4,27 @@
 
 using namespace std;
 
-int a[100005]; 
-long long dp[100005]; 
+int a[100005];
+long long int sum[100005], dp[100005];
 
-int main(){
+int main() {
     int T,N;
     cin>>T;
     while(T--){
         cin>>N;
-        for (int i = 0; i < n; ++i) 
+        for (int i = N - 1; i >= 0; i--)
             cin>>a[i];
+        sum[0] = a[0];
+        for (int i = 1; i < N; i++)
+            sum[i] = sum[i-1] + a[i];
 
-        long long sum = 0; 
-        dp[n] = dp[n + 1] = dp[n + 2] = 0; 
-        for (int i = n - 1; i >= 0; --i){ 
-            sum += a[i]; 
-            dp[i] = sum - min(min(dp[i + 1], dp[i + 2]), dp[i + 3]); 
-        }
-        cout<<dp[0]<<"\n"; 
+        dp[0] = sum[0];
+        dp[1] = sum[1];
+        dp[2] = sum[2];
+        for (int i = 3; i < N; i++)
+            dp[i] = max(sum[i]-dp[i-1],max(sum[i]-dp[i-2],sum[i]-dp[i-3]));
+
+        cout<<dp[N-1]<<"\n";
     }
-    return 0; 
+    return 0;
 }
---
+++ hackerrank/algo/search/icecream.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int T,m,n
+	return 0;
+}
