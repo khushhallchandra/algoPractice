52cc218d2604ad5f83675e34ff7f5624a53a5465
Khushhall <kcm00000@gmail.com>
2016-10-28 22:08:00 +0530

    Easy

---
+++ search/peak.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+ 
+int findPeak(int arr[], int low, int high, int n){
+    int mid = (low + high)/2; 
+ 
+    if ((mid == 0 || arr[mid-1] <= arr[mid]) && (mid == n-1 || arr[mid+1] <= arr[mid]))
+        return mid;
+
+    else if (mid > 0 && arr[mid-1] > arr[mid])
+        return findPeak(arr, low, (mid -1), n);
+ 
+    else return findPeak(arr, (mid + 1), high, n);
+}
+ 
+ 
+int main(){
+    int arr[] = {1, 3, 20, 14, 11, 0};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout<<findPeak(arr, 0, n-1, n)<<endl;
+    return 0;
+}
