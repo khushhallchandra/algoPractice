c29a62703eb945d00049bd9157805899337533c1
khushhallchandra <kcm00000@gmail.com>
2016-12-25 21:21:23 +0530

    added

---
+++ 30 Days Of Code/cycles.cpp
@@ -0,0 +1,26 @@
+/*
+Detect a cycle in a linked list. Note that the head pointer may be 'NULL' if the list is empty.
+
+A Node is defined as: 
+    struct Node {
+        int data;
+        struct Node* next;
+    }
+*/
+
+bool has_cycle(Node* head) {
+
+    if(!head || !(head -> next)) return false;
+    
+    Node * p = head;
+    Node * q = head;
+    
+    while(q && q -> next){
+        q = q -> next;                        
+        q = q -> next;
+        p = p -> next;
+        if(p == q) 
+        	return true;
+    }    
+    return false;
+}
